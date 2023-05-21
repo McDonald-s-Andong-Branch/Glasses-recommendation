10c0e56489a5352783905623598512f5a943706f
KIBA <kjh030529@gmail.com>
2023-05-21 15:49:49 +0900

    Add https Secure protocol

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const app = express();
+
+const HTTP_PORT = 8080;
+const HTTPS_PORT = 8443;
+
+const options = {
+    key: fs.readFileSync('./rootca.key'),
+    cert: fs.readFileSync('./rootca.crt')
+};
 
 app.use(express.static('public'));
 
@@ -7,6 +18,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, () => {
-    console.log('McDonald open (port : 8080)');
-});
+// Create an HTTP server.
+http.createServer(app).listen(HTTP_PORT);
+
+// Create an HTTPS server.
+https.createServer(options, app).listen(HTTPS_PORT);
