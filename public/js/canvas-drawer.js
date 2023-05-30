411f875003c3c3a7fa2f056581b3c0bc97890ae7
KIBA <kjh030529@gmail.com>
2023-05-31 02:02:40 +0900

    Fix Face position calculation for wearing glasses graphics

--- public/js/canvas-drawer.js
+++ public/js/canvas-drawer.js
@@ -15,3 +15,21 @@ function drawBox(ctx, rect) {
     ctx.lineWidth = 4;
     ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
 }
+
+function drawImage(canvas, image, centerX, centerY, angle) {
+    var ctx = canvas.getContext('2d');
+
+    var imageWidth = image.width;
+    var imageHeight = image.height;
+
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.rotate(angle);
+    ctx.drawImage(image, -imageWidth / 2, -imageHeight / 2, imageWidth, imageHeight);
+    ctx.restore();
+}
+
+function clearCanvas(canvas) {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
--- public/js/face_recog.js
+++ public/js/face_recog.js
@@ -10,6 +10,13 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
 ]).then(OnReadyFaceAPI);
 
+/* 이쪽이 안경 이미지 불러오는 코드입니다 */
+var canvas = document.getElementById('canvas');
+var glassesImage = new Image();
+glassesImage.src = 'assets/2.png';
+glassesImage.width = 300;
+glassesImage.height = 150;
+
 console.log("로드 끝");
 
 function OnReadyFaceAPI() {
@@ -28,16 +35,19 @@ function OnReadyFaceAPI() {
         context.drawImage(video, 0, 0, canvas.width, canvas.height); // 캡처한 이미지 그리기
 
         // 이미지를 네모 박스에 표시하기
-        var capturedImage = new Image();
-        capturedImage.src = canvas.toDataURL(); // 캔버스의 이미지를 데이터 URL로 가져옴
-        capturedImage.style.width = "100%";
-        capturedImage.style.height = "100%";
+        var newCanvas = document.createElement('canvas');
+        newCanvas.width = canvas.width;
+        newCanvas.height = canvas.height;
+        newCanvas.style.width = "100%";
+        newCanvas.style.height = "100%";
+        var ctx = newCanvas.getContext('2d');
+        ctx.drawImage(canvas, 0, 0);
 
         var imageContainer = document.getElementById("capturedImageContainer");
         imageContainer.innerHTML = ""; // 이미지를 표시하기 전에 기존 내용을 지움
-        imageContainer.appendChild(capturedImage);
+        imageContainer.appendChild(newCanvas);
 
-        AnalyzeFace(context, canvas);
+        AnalyzeFace(context, canvas, video, newCanvas, ctx);
     });
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -49,11 +59,16 @@ function OnReadyFaceAPI() {
     }
 }
 
-function AnalyzeFace(context, canvas) {
+function AnalyzeFace(context, canvas, video, ouputCanvas, ouputContext) {
     setInterval(async () => {
         const options = getFaceDetectorOptions()
         const detections = await faceapi.detectAllFaces(canvas, options).withFaceLandmarks();
-        console.log(detections);
+
+        clearCanvas(canvas);
+        clearCanvas(ouputCanvas);
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        ouputContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+
         detections.forEach((face) => {
             const box = face.detection.box;
             const landmarks = face.landmarks;
@@ -67,6 +82,31 @@ function AnalyzeFace(context, canvas) {
             drawPoints(context, nose);
 
             drawBox(context, box);
+
+            const ouput = calculateEyePositionAndFaceAngle(landmarks.positions);
+
+            drawImage(ouputCanvas, glassesImage, ouput.x, ouput.y, ouput.d);
         });
     }, 300);
+}
+
+function calculateEyePositionAndFaceAngle(landmarksData) {
+    const leftEyeIndex = 36; // 왼쪽 눈
+    const rightEyeIndex = 45; // 오른쪽 눈
+
+    const leftEye = landmarksData[leftEyeIndex];
+    const rightEye = landmarksData[rightEyeIndex];
+
+    // 양눈 사이 점 위치계산
+    const eyeCenterX = (leftEye.x + rightEye.x) / 2;
+    const eyeCenterY = (leftEye.y + rightEye.y) / 2;
+
+    // 얼굴의 기울기계산
+    const angle = Math.atan2(rightEye.y - leftEye.y, rightEye.x - leftEye.x);
+
+    return {
+        x: eyeCenterX,
+        y: eyeCenterY,
+        d: angle
+    };
 }
